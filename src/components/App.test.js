eab28e6667aa543b3ac49570c1a1c394c9211f39
Hieu Tran <hmtran.ae@gmail.com>
2019-02-08 14:56:38 -0500

    3 tests mentioned above in app.js

---
+++ src/components/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+configure({ adapter: new Adapter() });
+
+const app = shallow(<App />);
+
+it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+});
+
+it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+
+    expect(app.state().gifts).toEqual([{id: 1}]);
+});
